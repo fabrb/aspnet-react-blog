b36f57cccc7311268cdf6e48ec98c3a53fa46811
Fábio Barbosa <faiorb@outlook.com>
2024-09-15 18:45:06 -0300

    chore: changed docker configurations

--- fabarblog/Program.cs
+++ fabarblog/Program.cs
@@ -1,23 +1,37 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Registrar os serviços necessários, incluindo os controladores
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+// Configurar o DbContext com a conexão ao PostgreSQL
 builder.Services.AddDbContext<Context>(options =>
-		options.UseNpgsql(builder.Configuration.GetConnectionString("PostgressConnection")));
+		options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
+
+// Registrar serviços do repositório e outros serviços de dependência
+builder.Services.AddScoped<PostRepository>();
+builder.Services.AddScoped<PostService>();
 
 var app = builder.Build();
 
+// Configurações para ambiente de desenvolvimento, como o Swagger
 if (app.Environment.IsDevelopment())
 {
+	app.UseDeveloperExceptionPage();
 	app.UseSwagger();
 	app.UseSwaggerUI();
-	app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
+
+// Middleware de autorização (se necessário)
+app.UseAuthorization();
+
+// Mapeamento dos controladores para as rotas
+app.MapControllers();
 
 app.Run();
