6225cec36b78922617c2156dc3290296d361ee44
Fábio Barbosa <faiorb@outlook.com>
2024-09-17 17:22:06 -0300

    feat: usage os Either in services

--- fabarblog/Services/PostService.cs
+++ fabarblog/Services/PostService.cs
@@ -1,6 +1,7 @@
 using fabarblog.DTO;
 using fabarblog.Models;
 using fabarblog.Repository;
+using fabarblog.Utils;
 
 namespace fabarblog.Services;
 public class PostService(PostRepository postsRepository)
@@ -12,28 +13,28 @@ public class PostService(PostRepository postsRepository)
 		return await _postsRepository.SearchAllPosts();
 	}
 
-	public async void CreateNewPost(PostDTO post)
+	public Either<ErrorCall, SuccessCall> CreateNewPost(PostDTO post)
 	{
 		if (post.Title is null)
 		{
-			return;
+			return Either.Instanciate<ErrorCall, SuccessCall>(new ErrorCall { Message = "Post not created", Details = new Exception("Invalid title"), StatusCode = 400 });
 		}
 
 		if (post.Content is null)
 		{
-			return;
+			return Either.Instanciate<ErrorCall, SuccessCall>(new ErrorCall { Message = "Post not created", Details = new Exception("Invalid content"), StatusCode = 400 });
 		}
 
 		Post newPost = new Post
 		{
 			Id = Guid.NewGuid(),
-			Title = post.Title,
-			Content = post.Content,
+			Title = "",
+			Content = "post.Content",
 			CreatedAt = DateTime.Now
 		};
 
-		_postsRepository.IncludeNewPost(newPost);
+		// _postsRepository.IncludeNewPost(newPost);
 
-		return;
+		return Either.Instanciate<ErrorCall, SuccessCall>(new SuccessCall { Message = "Post created", Details = newPost, StatusCode = 201 });
 	}
 }
