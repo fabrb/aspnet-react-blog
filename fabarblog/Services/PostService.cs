acb8902eb0f10b8cff20bb4dcc1f8a98b76d4bee
Fábio Barbosa <faiorb@outlook.com>
2024-09-17 21:10:11 -0300

    feat: new service for users

--- fabarblog/Services/PostService.cs
+++ fabarblog/Services/PostService.cs
@@ -13,28 +13,24 @@ public class PostService(PostRepository postsRepository)
 		return await _postsRepository.SearchAllPosts();
 	}
 
-	public Either<ErrorCall, SuccessCall> CreateNewPost(PostDTO post)
+	public async Task<Either<ErrorCall, SuccessCall>> CreateNewPost(PostDTO post)
 	{
 		if (post.Title is null)
-		{
-			return Either.Instanciate<ErrorCall, SuccessCall>(new ErrorCall { Message = "Post not created", Details = new Exception("Invalid title"), StatusCode = 400 });
-		}
+			return Either.Instanciate<ErrorCall, SuccessCall>(new ErrorCall { Message = "Post not created", Details = new Exception("Invalid title") });
 
 		if (post.Content is null)
-		{
-			return Either.Instanciate<ErrorCall, SuccessCall>(new ErrorCall { Message = "Post not created", Details = new Exception("Invalid content"), StatusCode = 400 });
-		}
+			return Either.Instanciate<ErrorCall, SuccessCall>(new ErrorCall { Message = "Post not created", Details = new Exception("Invalid content") });
 
-		Post newPost = new Post
+		Post newPost = new()
 		{
-			Id = Guid.NewGuid(),
-			Title = "",
-			Content = "post.Content",
-			CreatedAt = DateTime.Now
+			Title = post.Title,
+			Content = post.Content,
+			CreatedAt = DateTime.UtcNow,
+			UserId = 2
 		};
 
-		// _postsRepository.IncludeNewPost(newPost);
+		await _postsRepository.IncludeNewPost(newPost);
 
-		return Either.Instanciate<ErrorCall, SuccessCall>(new SuccessCall { Message = "Post created", Details = newPost, StatusCode = 201 });
+		return Either.Instanciate<ErrorCall, SuccessCall>(new SuccessCall { Message = "Post created", Details = newPost });
 	}
 }
