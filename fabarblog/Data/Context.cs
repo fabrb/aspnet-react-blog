04640ac8058240e27ef9fcc72a9e372b1febd269
Fábio Barbosa <faiorb@outlook.com>
2024-09-17 21:11:08 -0300

    feat: add auto increment in Context

--- fabarblog/Data/Context.cs
+++ fabarblog/Data/Context.cs
@@ -1,10 +1,18 @@
 using fabarblog.Models;
 using Microsoft.EntityFrameworkCore;
 
+namespace fabarblog.Data;
+
 public class Context : DbContext
 {
 	public Context(DbContextOptions<Context> options) : base(options) { }
 
 	public DbSet<Post> Posts { get; set; }
 	public DbSet<User> Users { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Post>().Property(p => p.Id).ValueGeneratedOnAdd();
+		modelBuilder.Entity<User>().Property(p => p.Id).ValueGeneratedOnAdd();
+	}
 }
