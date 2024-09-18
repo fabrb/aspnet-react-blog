6bf5baa64a4a1ddc6acb7a7ef6a9ee2a3aefcdcf
Fábio Barbosa <faiorb@outlook.com>
2024-09-17 21:06:27 -0300

    feat: add new response classes

--- fabarblog/Utils/Responses/ErrorCall.cs
+++ fabarblog/Utils/Responses/ErrorCall.cs
@@ -1,7 +1,6 @@
 namespace fabarblog.Utils;
 public class ErrorCall
 {
-	public int StatusCode { get; set; }
 	public string Message { get; set; }
 	public Exception Details { get; set; }
 }
--- fabarblog/Utils/Responses/SuccessCall.cs
+++ fabarblog/Utils/Responses/SuccessCall.cs
@@ -2,7 +2,6 @@ namespace fabarblog.Utils;
 
 public class SuccessCall
 {
-	public int? StatusCode { get; set; }
 	public string Message { get; set; }
 	public Object Details { get; set; }
 }
