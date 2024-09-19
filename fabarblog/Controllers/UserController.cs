4b8270e0e92c6579447eab19016ddc8095fd42b9
Fábio Barbosa <faiorb@outlook.com>
2024-09-19 20:58:19 -0300

    feat: add simple first steps for authentication validation

--- fabarblog/Controllers/UserController.cs
+++ fabarblog/Controllers/UserController.cs
@@ -1,18 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using fabarblog.DTO;
 using fabarblog.Models;
 using fabarblog.Services;
-using System.Diagnostics;
-using fabarblog.Utils;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 
 namespace fabarblog.Controller;
 
 [ApiController]
 [Route("api/user")]
-public class UserController(UserService usersService) : ControllerBase
+public class UserController(UserService usersService, IConfiguration configuration) : ControllerBase
 {
 	private readonly UserService _usersService = usersService;
+	private readonly IConfiguration _configuration = configuration;
 
+	[Authorize(Roles = "admin")]
 	[HttpGet]
 	public async Task<ActionResult<IEnumerable<User>>> ListAll()
 	{
@@ -20,6 +24,7 @@ public class UserController(UserService usersService) : ControllerBase
 		return Ok(users);
 	}
 
+	[Authorize]
 	[HttpPost]
 	public async Task<ActionResult<Guid>> Create([FromBody] UserDTO user, [FromHeader] string? autenthication)
 	{
@@ -32,5 +37,37 @@ public class UserController(UserService usersService) : ControllerBase
 			return BadRequest(result);
 
 		return Ok(result);
+	}
+
+	[AllowAnonymous]
+	[HttpPost("login")]
+	public async Task<ActionResult<Guid>> Authenticate([FromBody] UserDTO user, [FromHeader] string? autenthication)
+	{
+		var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+		var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+		var tokenDescriptor = new SecurityTokenDescriptor
+		{
+			Subject = new ClaimsIdentity(
+			[
+				new(ClaimTypes.NameIdentifier, user.Email),
+				new(ClaimTypes.Name, user.Username),
+				new(ClaimTypes.Role, "admin")
+			]),
+
+			Expires = DateTime.UtcNow.AddHours(1),
+			SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+		};
+
+		var token = tokenHandler.CreateToken(tokenDescriptor);
+		var tokenString = tokenHandler.WriteToken(token);
+
+		return Ok(new { Token = tokenString });
+	}
+
+	[Authorize]
+	[HttpPut("logout")]
+	public async Task<ActionResult<Guid>> Unauthenticate([FromHeader] string? autenthication)
+	{
+		return Ok();
 	}
 }
