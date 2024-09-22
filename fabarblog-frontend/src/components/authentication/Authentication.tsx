44a86d44bbf54add3f50d5ca5f946f3e7f0d936b
Fábio Barbosa <faiorb@outlook.com>
2024-09-22 14:48:54 -0300

    feat: add initial validations for auth

--- fabarblog-frontend/src/components/authentication/Authentication.tsx
+++ fabarblog-frontend/src/components/authentication/Authentication.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Authentication: React.FC = () => {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [username, setUsername] = useState("");
+
+	useEffect(() => {
+		const authToken = localStorage.getItem("auth.token")
+		const authUser = localStorage.getItem("auth.username")
+		setUsername(authUser || "")
+
+		if (authToken)
+			setIsAuthenticated(true)
+	}, []);
+
+	function UserBlock() {
+		if (isAuthenticated)
+			return <><i className="fas fa-user pe-1"></i>{username}</>
+
+		return <><i className="fas fa-user pe-1"></i>Login</>
+	}
+
 	return (
 		<div className='px-4'>
-			<i className="fas fa-user"></i> User
-		</div>
+			<UserBlock />
+		</div >
 	);
 };
 
--- fabarblog-frontend/src/components/navigation/Navigation.tsx
+++ fabarblog-frontend/src/components/navigation/Navigation.tsx
@@ -7,8 +7,7 @@ const Navigation: React.FC = () => {
 		<div id='navigation' className='d-flex justify-content-between bg-dark text-bg-primary m-0 py-2'>
 			<ul className='d-flex flex-row m-0 px-4'>
 				<li className='px-1'><Link to="/">Home</Link></li>
-				<li className='px-1'><Link to="/post">Post</Link></li>
-				<li className='px-1'><Link to="/post2">Post2</Link></li>
+				<li className='px-1'><Link to="/write">Write</Link></li>
 				<li className='px-1'><Link to="/user/100">User</Link></li>
 			</ul>
 
