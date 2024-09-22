65417229c22368fdf3feb5a1a7a526d2ac8fb956
Fábio Barbosa <faiorb@outlook.com>
2024-09-21 21:13:38 -0300

    fix: added role for response user

--- fabarblog/DTO/User/UserResponse.cs
+++ fabarblog/DTO/User/UserResponse.cs
@@ -1,9 +1,12 @@
+using System.Runtime.InteropServices;
+
 namespace fabarblog.DTO;
 public class UserResponse
 {
 	public int Id { get; set; }
 	public string Username { get; set; }
 	public string Email { get; set; }
+	public string? Role { get; set; }
 	public DateTime CreationDate { get; set; }
 
 }
