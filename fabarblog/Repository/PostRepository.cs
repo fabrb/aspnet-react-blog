9a9150c205207ed7fc568b7c7e49d2d12948a006
Fábio Barbosa <faiorb@outlook.com>
2024-09-21 21:15:09 -0300

    fix: add user information on post search

--- fabarblog/Repository/PostRepository.cs
+++ fabarblog/Repository/PostRepository.cs
@@ -25,37 +25,43 @@ public class PostRepository
 	public async Task<Post?> SearchPost(int id)
 	{
 		return await _context.Posts
+			.Include(u => u.User)
 			.FirstOrDefaultAsync(post => post.Id == id);
 	}
 
-	public async Task<bool> IncludeNewPost(Post post)
+	public async Task<Post> IncludeNewPost(Post post)
 	{
+		Post x = new();
+
 		try
 		{
-			var x = await _context.Posts.AddAsync(post);
+			var entityEntry = await _context.Posts.AddAsync(post);
 			await _context.SaveChangesAsync();
+
+			x = entityEntry.Entity;
 		}
 		catch (Exception ex)
 		{
 			Console.WriteLine(ex);
 		}
 
-		return true;
+		return x;
 	}
 
-	public async Task<bool> EditPost(Post post)
+	public async Task<Post> EditPost(Post post)
 	{
 		try
 		{
-			var x = _context.Posts.Entry(post).State = EntityState.Modified;
+			_context.Posts.Entry(post).State = EntityState.Modified;
 			await _context.SaveChangesAsync();
+
 		}
 		catch (Exception ex)
 		{
 			Console.WriteLine(ex);
 		}
 
-		return true;
+		return post;
 	}
 
 	public async Task<bool> RemovePost(Post post)
@@ -68,6 +74,7 @@ public class PostRepository
 		catch (Exception ex)
 		{
 			Console.WriteLine(ex);
+			return false;
 		}
 
 		return true;
